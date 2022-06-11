41aa13c6fadfb7d141ff9f0eb75d1155c0d2578e
Sergey <skvorcovsa90@gmail.com>
2022-06-11 14:43:36 +1000

    drag_drop

---
+++ app.js
@@ -0,0 +1,37 @@
+const item = document.querySelector(".item");
+const placeholders = document.querySelectorAll(".placeholder");
+
+item.addEventListener("dragstart", dragstart);
+item.addEventListener("dragend", dragend);
+
+placeholders.forEach((placeholder) => {
+  placeholder.addEventListener("dragover", dragover);
+  placeholder.addEventListener("dragenter", dragenter);
+  placeholder.addEventListener("dragleave", dragleave);
+  placeholder.addEventListener("drop", dragdrop);
+});
+
+function dragstart(ev) {
+  ev.target.classList.add("hold");
+  setTimeout(() => {
+    ev.target.classList.add("hide");
+  }, 0);
+}
+
+function dragend(ev) {
+  ev.target.classList.remove("hold", "hide");
+}
+
+function dragover(ev) {
+  ev.preventDefault();
+}
+function dragenter(ev) {
+  ev.target.classList.add("hovered");
+}
+function dragleave(ev) {
+  ev.target.classList.remove("hovered");
+}
+function dragdrop(ev) {
+  ev.target.classList.remove("hovered");
+  ev.target.append(item);
+}
